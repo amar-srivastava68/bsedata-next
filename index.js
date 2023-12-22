01a208f1189a1909719189e3fac25fa8541873c6
Amar Srivastava <70334464+amarsrivastava68@users.noreply.github.com>
2023-12-22 09:21:51 +0530

    testing file index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import mysql from "mysql2";
+
+function creatingConnection() {
+  const connection = mysql.createConnection({
+    host: "64.227.174.65",
+    user: "amar",
+    password: "12345678",
+    database: "bse",
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL:", err);
+      return;
+    }
+    console.log("Connected to MySQL database");
+  });
+}
+
+creatingConnection();
